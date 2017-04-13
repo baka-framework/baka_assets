bfa5154b13563c890622f5bf996234cfa372412b
Nanang Suryadi <nanang.jobs@gmail.com>
2017-04-13 14:25:46 +0700

    add assets in core baka_assets

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='baka_assets',
-      version='0.3.6.dev4',
+      version='0.3.7',
       description='Assets for Baka and Pyramid',
       long_description=__doc__,
       author='Nanang Suryadi',
